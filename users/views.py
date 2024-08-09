62798aacbb207191ad335c9b68991b49e184050d
Vishnu021198 <nair654065@gmail.com>
2024-08-09 09:55:05 +0530

    Final Version

--- users/views.py
+++ users/views.py
@@ -29,14 +29,3 @@ class LoginView(APIView):
                 })
             return Response({'detail': 'Invalid credentials'}, status=status.HTTP_401_UNAUTHORIZED)
         return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
-    
-
-class LogoutView(APIView):
-    def post(self, request, *args, **kwargs):
-        try:
-            refresh_token = request.data["refresh"]
-            token = RefreshToken(refresh_token)
-            token.blacklist()
-            return Response(status=status.HTTP_205_RESET_CONTENT)
-        except Exception as e:
-            return Response(status=status.HTTP_400_BAD_REQUEST)
